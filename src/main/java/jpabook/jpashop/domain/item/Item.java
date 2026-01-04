72f0fe535de9c86602105b454e1b4b9d60ebc2ea
hyen <hyen20719@gmail.com>
2026-01-04 17:32:13 +0900

    [jpa1-6] 상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn
 public abstract class Item {
@@ -24,4 +25,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //== 비즈니스 로직(엔티티 안에 비즈니스로직 넣는게 객체지향적으로 좋음==//
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int resetStock = this.stockQuantity - quantity;
+        if(resetStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = resetStock;
+    }
 }
