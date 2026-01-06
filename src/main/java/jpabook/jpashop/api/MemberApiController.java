fb54fe2afa5759681cca2bbdd6ca10de9d3fc369
hyen <hyen20719@gmail.com>
2026-01-06 20:53:43 +0900

    [jpa2-2-3] 회원조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,12 +8,55 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController // @Controller + @ResponseBody
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
 
+    /**
+     * 회원 조회 API
+     * - 응답 값으로 엔티티 반환
+     * => 엔티티 모든 값이 노출되는 단점, 향후 API 스펙 변경이 어려움
+     * => 응답 스펙을 맞추기 위해 로직이 추가됨. (@JsonIgnore, 별도의 뷰 로직 등등)
+     * @return
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    /**
+     * 회원 조회 API
+     * - 응답 값으로 엔티티가 아닌 별도의 DTO를 반환
+     * @return
+     */
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        //엔티티 -> DTO 변환
+        Collection<Object> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public class MemberDto{
+        private String name;
+    }
     /**
      * 회원 등록api
      * @param member
@@ -25,6 +68,12 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * 회원 등록 api
+     * (DTO로 반환)
+     * @param request
+     * @return
+     */
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid CreateMemberRequest request) {
 
