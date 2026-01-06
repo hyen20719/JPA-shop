67fa0d5c9e58d8b83da02e474a28271cdb6eabdc
hyen <hyen20719@gmail.com>
2026-01-06 20:05:26 +0900

    [jpa2-2-2] 회원수정 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,11 +3,10 @@ package jpabook.jpashop.api;
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // @Controller + @ResponseBody
 @RequiredArgsConstructor
@@ -15,6 +14,11 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    /**
+     * 회원 등록api
+     * @param member
+     * @return
+     */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
@@ -29,6 +33,34 @@ public class MemberApiController {
         Long id = memberService.join(member);
 
         return new CreateMemberResponse(id);
+    }
+
+    /**
+     * 회원 수정api
+     * @param id
+     * @param request
+     * @return
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2
+            (@PathVariable("id") Long id,
+             @RequestBody @Valid UpdateMemberRequest request){
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
     }
 
     @Data
